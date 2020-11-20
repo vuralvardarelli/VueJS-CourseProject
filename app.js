7a95ed86169ba2a13c6f5fff907bd9742f336cc0
Vural Vardarelli <vuralvardarelli@hotmail.com>
2020-11-20 14:43:50 +0300

    all functionality, just need logs

--- app.js
+++ app.js
@@ -7,18 +7,43 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            currentRound: 0
+            currentRound: 0,
+            winner: null
         };
     },
     computed: {
         monsterBarStyles() {
+            if (this.monsterHealth < 0)
+                return { width: '0%' };
             return { width: this.monsterHealth + '%' };
         },
         playerBarStyles() {
+            if (this.playerHealth < 0)
+                return { width: '0%' };
             return { width: this.playerHealth + '%' };
         },
         mayUseSpecialAttack() {
             return this.currentRound % 3 !== 0;
+        }
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                //a draw
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                //player lost
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                //a draw
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                //monster lost
+                this.winner = 'player';
+            }
         }
     },
     methods: {
@@ -44,6 +69,15 @@ const app = Vue.createApp({
             this.playerHealth += healVal;
             if (this.playerHealth > 100) this.playerHealth = 100;
             this.attackPlayer();
+        },
+        startNewGame() {
+            this.currentRound = 0;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.winner = null;
+        },
+        surrender() {
+            this.winner = 'monster';
         }
     }
 });
